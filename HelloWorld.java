85c3163a69c06908acbdd907ccf584c8c163ad98
Jamie Watson <50807261+JamieWatson89@users.noreply.github.com>
2019-10-03 10:01:58 +0200

    Update HelloWorld.java
    \\Method created to print "Hello World"

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
+\\Method created to print "Hello World"
+
 class HelloWorld {
+  
+  static void print(){
+  System.out.println("Hello world!");
+  }
+  
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    print();
+    
   }
 }
